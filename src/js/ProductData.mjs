c8eadeab116f42d5b1ca81ba1cf37a7a2b36772f
Mattvil04 <matisebastianvillacis@gmail.com>
2025-12-09 11:24:20 -0500

    update for w2

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -6,18 +6,17 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ProductData  {
   constructor(category) {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+      .then(convertToJson).then((data) => data);
   }
   async findProductById(id) {
-    const products = await this.getData();
+    const products = await this.getData()
     return products.find((item) => item.Id === id);
   }
 }
